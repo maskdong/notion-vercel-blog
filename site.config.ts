c452df1d09718e68415787df7f4202180ab1a295
maskdong <143996780+maskdong@users.noreply.github.com>
2024-03-05 11:40:28 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -18,7 +18,7 @@ export default siteConfig({
 
   // social usernames (optional)
   // twitter: 'transitive_bs',
-  github: 'https://github.com/maskdong',
+  github: 'maskdong',
   // linkedin: 'fisch2',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
